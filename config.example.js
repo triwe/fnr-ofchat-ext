fa0dbdfdc9852fe0d87d78ab98e823760b1c7872
tristan <105899557+triwe@users.noreply.github.com>
2024-09-03 10:27:42 -0700

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -2,10 +2,11 @@
 // Create a config.js file that contains the following:
 
 
-const CONFIG = {
-    NOTION_API_KEY: 'your-integration-token',
-    FEEDBACK_DATABASE_ID: 'your-feedback-database-id',
-    REQUEST_DATABASE_ID: 'your-request-database-id'
-};
-
-export default CONFIG;
+if (!window.CONFIG) {
+    window.CONFIG = {
+        NOTION_API_KEY: 'your_key_here',
+        FEEDBACK_DATABASE_ID: 'your_key_here', // Correct UUID format
+        REQUEST_DATABASE_ID: 'your_key_here',
+        GOOGLE_API_KEY: 'your_key_here',
+    };
+}
